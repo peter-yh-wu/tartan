0c724870b38646b504ed6b59ac71cedb594ad07f
peterwu99 <peter.yh.wu@gmail.com>
2018-02-10 18:25:20 -0500

    sent.py

--- speech/sent.py
+++ speech/sent.py
@@ -16,16 +16,41 @@ authentication.set_access_token(
 )
 api = tweepy.API(authentication)
 
-def yose_emot:
-    '''51270 words'''
-    score = 0
-    count = 0
-    for tweet in tweepy.Cursor(api.user_timeline,id='YosemiteNPS').items():
-        for word in tweet.text.split():
+def yose_emot(userid):
+    '''
+    returns score for the sentiment of given twitter feed
+    '''
+    score = 0.0
+    count = 0.0
 
-            s0 = swn.senti_synsets(word,'a')
-            s1 = list(s0)
-            if len(s1) > 0:
-                 score += s1[0].pos_score()
-                 count += 1
+    for tweet in tweepy.Cursor(api.user_timeline,id=userid).items():
+        ctxt = tweet.text
+        if len(ctxt) > 0:
+            cscore = emot_phrase(ctxt)
+            if cscore != -1:
+                score += cscore
+                count += 1.0
+
     return (str(score/count))
+
+def emot_phrase(phrase):
+    '''
+    returns a score for the sentiment of the given phrase
+    if the phrase is an empty string, returns -1
+    '''
+    score = 0.0
+    count = 0.0
+
+    for word in phrase.split():
+        s0 = swn.senti_synsets(word,'a')
+        s1 = list(s0)
+        if len(s1) > 0:
+             score += s1[0].pos_score()
+             count += 1.0
+
+    if count == 0.0:
+        return -1
+    else:
+        return (score/count)
+
+print(str(yose_emot("YosemiteNPS")))
