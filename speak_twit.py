7f6c822f8194b14f8682f4ee3da56bce88c12675
peterwu99 <peter.yh.wu@gmail.com>
2018-02-10 03:13:24 -0500

    twitter to speech

--- speak_twit.py
+++ speak_twit.py
@@ -4,6 +4,11 @@ import os
 import twitter
 from gtts import gTTS
 
+api = twitter.Api(consumer_key='7r2eXtOYae9rh73hntdmykKa7',
+  consumer_secret='SiMKTMFvwTZDcwbBstzggOprAE2LNMblN8PIn9PmSONJNseQg0',
+    access_token_key='962236458202517504-qTNen7xpBuo5RsDwQPSw3Qw3oRpYFxD',
+    access_token_secret='VsEOPcSZ51NG07kdWXoqQPn24JetNsRBlO2PmE1DghxYt')
+
 statuses = api.GetUserTimeline(screen_name='Beyonce') # user id
 message_00 = statuses[0].text
 
